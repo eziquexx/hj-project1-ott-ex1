a477e3c88271fdceb3ff7d3edb32d1c9d87a836d
eziquexx <waftyann@gmail.com>
2024-10-05 01:03:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/ysedu/hj/ott/YseduHjOttEx1Application.java
@@ -0,0 +1,13 @@
+package com.ysedu.hj.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YseduHjOttEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YseduHjOttEx1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ysedu/hj/ott/YseduHjOttEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysedu.hj.ott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YseduHjOttEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
